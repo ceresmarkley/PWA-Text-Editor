ad447039e18fe147fbb0ed0d2e769dbf144155d6
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-09-17 11:28:23 -0400

    created function code for registerRoute which will allow asset caching up to 30 days. Created offlineFallback function for when page is not found while offline.

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'image', 'worker'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        // cache responses with max of 30 days
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      }),
+    ],
+  })
+);
+
+// offlineFallback will return the index.html page if a page is not found in the cache.
+offlineFallback({ pageFallback: '/index.html' });
