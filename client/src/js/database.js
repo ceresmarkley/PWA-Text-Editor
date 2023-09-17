a4070aea9c9310ffbe84bda570241a792355f6fe
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-09-17 11:07:15 -0400

    created getDB and pout DB functions.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  // use openDB to get into jate database version 1 
+  const jateDb = await openDB('jate', 1);
+  // create a read and write transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // using put method to pass data(id and value) into request
+  const request = store.put({ value: content });
+  const result = await request;
+  console.log('📝 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all content from the database');
+  // use openDB to get into jate database version 1 
+  const jateDb = await openDB('jate', 1);
+  // created a read ONLY transaction
+  const tx = jateDb.transaction('jate', 'readonly');
+  // open jate object store
+  const store = tx.objectStore('jate');
+  // use getAll method to get data into request
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  // add .value to get back so that app will show the stored data
+  return result.value;
+};
 
 initdb();
