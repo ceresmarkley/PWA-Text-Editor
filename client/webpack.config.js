c35e8c5d0b5d349a1d2e64712224ed26b000cfb7
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-09-17 11:55:42 -0400

    configured workbox plugins for service worker and manifest file with InjectManifest and webpackpwamanifest.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Just Another Text Editor(JATE)",
+      }),
+      // service-worker.js
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // create manifest.json
+      new WebpackPwaManifest({
+        // I don't want to get manifest.<fingerprint>.json
+        fingerprints: false,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'offline text editor',
+        background_color: '#497bd1',
+        theme_color: '#497bd1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // css-loader
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // babel
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
